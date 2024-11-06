d5d13541b72d49f73f7421a3dd04df815812c99b
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-06 16:51:42 +0900

    ✨ [FEAT] 엔티티 작성

---
+++ src/main/java/com/blueDragon/Convenience/Model/ProductComment.java
@@ -0,0 +1,26 @@
+package com.blueDragon.Convenience.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Getter
+@Builder
+public class ProductComment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 256, nullable = false)
+    private String comment; //댓글 본문
+
+    //생성 시간은 baseEntity에 존재 (중복되는 코드 줄이기 위함)
+
+}
