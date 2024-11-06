972a4fff6def1e40a917f7ea6c431cd265bb9808
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-06 16:56:40 +0900

    🗃️ [ENTITY] 엔티티 생성

---
+++ src/main/java/com/blueDragon/Convenience/Model/User.java
@@ -0,0 +1,25 @@
+package com.blueDragon.Convenience.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id", nullable = false)
+    private String loginId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+}
