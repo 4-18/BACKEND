26fae053e976f9de7c8d1254d0d746d27c1821fc
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-08 19:43:33 +0900

    ✨ [FEAT] 상품 좋아요 재구현, 싫어요 제거

---
+++ src/main/java/com/blueDragon/Convenience/Model/RecommendLike.java
@@ -0,0 +1,12 @@
+package com.blueDragon.Convenience.Model;
+
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class RecommendLike extends ReactionBaseEntity{
+//    @ManyToOne
+//    @JoinColumn(name = "Product")
+//    private Product product;
+}
