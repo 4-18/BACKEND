6f369bd92a169a379fa152cd414376e4a649726d
kdhdd <kdso10@naver.com>
2024-11-16 01:37:11 +0900

    :sparkles:[FEAT] 마이페이지(회원 조회) 구현

--- src/main/java/com/blueDragon/Convenience/Service/UserService.java
+++ src/main/java/com/blueDragon/Convenience/Service/UserService.java
@@ -1,13 +1,17 @@
 package com.blueDragon.Convenience.Service;
 
 import com.blueDragon.Convenience.Dto.User.RegisterUserDto;
+import com.blueDragon.Convenience.Dto.User.ResponseUserDto;
 import com.blueDragon.Convenience.Exception.DuplicateLoginIdException;
+import com.blueDragon.Convenience.Exception.UserNotExistException;
 import com.blueDragon.Convenience.Model.User;
 import com.blueDragon.Convenience.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -32,5 +36,12 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    public ResponseUserDto getUsernameAndNickname(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotExistException("존재하지 않는 유저입니다."));
+
+        return ResponseUserDto.entityToDto(user);
     }
 }
