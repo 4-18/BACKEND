7225e9d56d0d8bf9930b9224164c1b42128e8211
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-15 19:14:22 +0900

    Update SecurityConfig.java

--- src/main/java/com/blueDragon/Convenience/Config/SecurityConfig.java
+++ src/main/java/com/blueDragon/Convenience/Config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable);
-
+        http.cors();
         http.formLogin(AbstractHttpConfigurer::disable);
 
         http.httpBasic(AbstractHttpConfigurer::disable);
